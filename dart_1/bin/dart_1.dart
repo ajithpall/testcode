7a188c0653fdf748023710527db6b94f299928d9
ajithpall <76776886+ajithpall@users.noreply.github.com>
2023-11-08 18:42:34 +0530

    Add files via upload

---
+++ dart_1/bin/dart_1.dart
@@ -0,0 +1,31 @@
+void main ()
+{
+  int ajith_pall = 10 ;
+  //this is a interger variable in dart programming 
+  double ajith = 3.4 ;; 
+  ///this is dart commants for multiple line   in dart there are two types of 
+  ///multiple comments one is c# style another one is java style now in the line is 
+  ///build buy c# style 
+  String ajith1 ="mentel";
+  /*this is one of the multiline comments mostly used in java top of the
+  statement is string data type */
+  bool ajith2 = false ;
+  var ajith3 = "sundal";
+  dynamic ajith4 = "junni";
+  print(ajith4);
+  ajith4 = 67;
+print(ajith_pall);
+print (ajith);
+print (ajith1);
+print (ajith3);
+print (ajith2);
+print(ajith4);
+int a = 10;
+int b = 1000;
+var c= a+b;
+print ("value of a and b is $c");
+var d = c -a ;
+print ("value of c - b is $d");
+var e= -d;
+print ("value of -d is $e");
+}
